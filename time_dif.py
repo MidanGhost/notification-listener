de3230097fb81167dfcd914baf6575844aa299fa
MidanGhost <yazansodan@gmail.com>
2022-12-25 00:12:09 +0800

    #

--- time_dif.py
+++ time_dif.py
@@ -4,14 +4,7 @@ from pywebio.session import *
 from pywebio import start_server
 from datetime import datetime
 
-# passes = {
-#             "CBJ-couple": 120,
-#             "CBJ-single": 120,
-#             "GGS-couple": 60,
-#             "GGS-single": 60,
-#             "MLK-couple": 60,
-#             "MLK-single": 60
-#         }
+#passes list 
 passes1 = {
     "CBJ-couple": {
         'time': 120
@@ -47,11 +40,13 @@ def time():
                 PlaceHolder="Scooter/ Bike ",
                 datalist=['scooter', 'bike']),
 
-            input('Penalty RM (Optional)', name='penalty', value=0 ,min=0, type=NUMBER),
-
             input('Pass (Optional)', name='pass', type= TEXT, 
                 PlaceHolder="Pass ",
-                datalist=["CBJ-couple","CBJ-Single","GGS-couple","GGS-single","MLK-couple","MLK-single"]),
+                datalist=["CBJ-couple","CBJ-Single","GGS-couple","GGS-single","MLK-couple","MLK-single"]),    
+
+            input('Penalty RM (Optional)', name='penalty', value=0 ,min=0, type=NUMBER),
+
+            
         ])
 
 
@@ -68,6 +63,7 @@ def time():
 
         start_time = datetime.strptime(data['start'], "%H:%M")
         end_time = datetime.strptime(data['end'], "%H:%M")
+
         # get difference
         delta = end_time - start_time
         
@@ -75,24 +71,34 @@ def time():
         sec = delta.total_seconds()
         # Convert the difference to M-sec
         mSec = sec/1000
-        
+        # sec to min
+        min1= sec/60
+        min= sec/60
         
         # passes check 
         if data['pass'] in passes1:
             
             pass_time= passes1[data['pass']]['time']
-            print(type(data['pass']))
-            min = (sec/60) - pass_time
-            pass_text = 'Pass type: ' + data['pass'] +' '+str(pass_time)+' min'
+            #pass time left
+            if min < pass_time:
+                min = pass_time - min1
+                time_status = '(Left)'
+            #pass time extra    
+            else:
+                min = min1 - pass_time
+                time_status = '(Extra)'
+            #display the pass info 
+            pass_text = '\nPass type: ' + data['pass'] +' '+str(pass_time)+' min'+' '
         else:
-            # sec to min
-            min = sec / 60
+            
+            time_status = ''
             pass_text=''
+            
 
 
         if min <=15 and str(data['type']) == "bike" :
             # First 15 min for RM2
-            trip_fare =2
+            trip_fare = 2
         else:
             trip_fare = (min - first_min)* price + first_min_price 
         
@@ -110,22 +116,32 @@ def time():
 
             fare_total= trip_fare + data['penalty']
 
-            fine_text= 'Total min with penalty: ' + str(fine) + '\nTrip fare with penalty: RM' +str(round(fare_total,4))
+            fine_text= '\nTotal min with penalty: ' + str(fine) + '\nTrip fare with penalty: RM' +str(round(fare_total,4))
 
         else:  
             fine_text= ''
 
+        if time_status == '(Left)':
+            fare_text = ''
+        else:    
+            fare_text = '\nTrip Fare: RM' + str(round(trip_fare,4))  
+
         v_type=["scooter","bike"]
         #result
-        if str(data['type']) in v_type and min>0 : 
+        if str(data['type']) in v_type and min>=0 : 
                 popup("Trip Details",
-                    f"Start time: {data['start']}\nEnd time: {data['end']}\
-                    \nType: {str(data['type'])}\nDuration: {full_time} {min} min || {format(mSec, '.6f')} M-sec\nTrip Fare: RM{round(trip_fare,4)}\n{pass_text}\n{fine_text}",
+                    f"Start time: {data['start']}\
+                    \nEnd time: {data['end']}\
+                    \nType: {str(data['type'])}\
+                    \nDuration: {full_time} {min} min {time_status} || {format(mSec, '.6f')} M-sec\
+                    {fare_text}\
+                    {pass_text}\
+                    {fine_text}",
                     closable=True )
         
         else:   
             #Validation for negative Time         
-            if min <=0:
+            if min <0:
                  toast('Negative Time is not allowed', position='center', color='red', duration=2)
             else:
                 #Validation for check the type 
